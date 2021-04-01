769faf5ac5d9e0fc5de4810538726a9a34e70061
Say Cheong <greymistcube@gmail.com>
2021-04-01 16:11:37 +0900

    dummy Transaction added; minor typo fix

--- ToyBlockChain/BlockHeader.cs
+++ ToyBlockChain/BlockHeader.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace BlockHeader
+namespace ToyBlockChain
 {
     public class BlockHeader
     {
---
+++ ToyBlockChain/Transaction.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace ToyBlockChain
+{
+    public class Transaction
+    {
+        public string Sender;
+        public string Recipient;
+        public float Value;
+        public int Timestamp;
+
+        public Transaction(
+            int timestamp,
+            string sender,
+            string recipient,
+            float value)
+        {
+            Timestamp = timestamp;
+            Sender = sender;
+            Recipient = recipient;
+            Value = value;
+        }
+
+        public byte[] HashBytes()
+        {
+            SHA256 sha256 = SHA256.Create();
+            return sha256.ComputeHash(Serialize());
+        }
+
+        public string HashString()
+        {
+            return BitConverter.ToString(HashBytes()).Replace("-", "");
+        }
+
+        public byte[] Serialize()
+        {
+            return Encoding.UTF8.GetBytes(ToString());
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0},{1},{2},{3}",
+                                 Timestamp, Sender, Recipient, Value);
+        }
+    }
+}
