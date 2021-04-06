061e278785f59ccf5d95cfbaa54666b6b2dd8bcc
Say Cheong <greymistcube@gmail.com>
2021-04-07 00:54:55 +0900

    script scaffolding

---
+++ ToyBlockChain.Script/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ToyBlockChain.Service;
+
+namespace ToyBlockChain.Script
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+
+            client.Run();
+        }
+    }
+}
