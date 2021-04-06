744c22db640a5389ea4f1ebd670bc93fb0543e08
Say Cheong <greymistcube@gmail.com>
2021-04-06 18:37:07 +0900

    transaction pool wrapping class added

--- ToyBlockChain.Service/Node.cs
+++ ToyBlockChain.Service/Node.cs
@@ -4,5 +4,13 @@ namespace ToyBlockChain.Service
 {
     public class Node
     {
+        private BlockChain _chain;
+        private TransactionPool _pool;
+
+        public Node()
+        {
+            _chain = new BlockChain();
+            _pool = new TransactionPool();
+        }
     }
 }
