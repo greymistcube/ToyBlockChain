173a77c1fee298dc732b4327caf63899099a454d
Say Cheong <greymistcube@gmail.com>
2021-04-07 13:59:22 +0900

    address book added to Node

--- ToyBlockChain.Service/Node.cs
+++ ToyBlockChain.Service/Node.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using ToyBlockChain.Core;
 
 namespace ToyBlockChain.Service
@@ -6,12 +7,27 @@ namespace ToyBlockChain.Service
     public class Node
     {
         private BlockChain _chain;
+        private HashSet<string> _addresses;
         private TransactionPool _pool;
 
         public Node()
         {
             _chain = new BlockChain();
+            _addresses = new HashSet<string>();
             _pool = new TransactionPool();
+        }
+
+        public void RegisterAddress(string address)
+        {
+            if (_addresses.Contains(address))
+            {
+                throw new ArgumentException();
+            }
+            else
+            {
+                _addresses.Add(address);
+            }
+            return;
         }
     }
 }
