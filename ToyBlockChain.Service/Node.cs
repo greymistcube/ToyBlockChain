fc55017d0949116b62f4fce3378ecd69123050b1
Say Cheong <greymistcube@gmail.com>
2021-04-07 15:28:21 +0900

    TransactionPool class removed; simply implemented as a dictionary inside Node class; several helper methods added to Node

--- ToyBlockChain.Service/Node.cs
+++ ToyBlockChain.Service/Node.cs
@@ -7,27 +7,93 @@ namespace ToyBlockChain.Service
     public class Node
     {
         private BlockChain _chain;
-        private HashSet<string> _addresses;
-        private TransactionPool _pool;
+        private HashSet<string> _book;
+        private Dictionary<string, Transaction> _pool;
 
         public Node()
         {
             _chain = new BlockChain();
-            _addresses = new HashSet<string>();
-            _pool = new TransactionPool();
+            _book = new HashSet<string>();
+            _pool = new Dictionary<string, Transaction>();
         }
 
+        public void AddBlock(Block block)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks if given address is in the book.
+        /// </summary>
+        public bool HasAddressInBook(string address)
+        {
+            return _book.Contains(address);
+        }
+
+        /// <summary>
+        /// Registers an address to the book.
+        /// </summary>
         public void RegisterAddress(string address)
         {
-            if (_addresses.Contains(address))
+            if (HasAddressInBook(address))
             {
-                throw new ArgumentException();
+                throw new ArgumentException("given address already exists");
             }
             else
             {
-                _addresses.Add(address);
+                _book.Add(address);
             }
-            return;
+        }
+
+        /// <summary>
+        /// Checks if given transaction is in the chain.
+        /// Mainly used to prevent the same transaction getting
+        /// added to the chain more than once.
+        /// </summary>
+        public bool HasTransactionInChain(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Checks if given transaction is in the pool.
+        /// Mainly used by a client to see if its generated transaction
+        /// has been processed into the chain.
+        /// </summary>
+        public bool HasTransactionInPool(Transaction transaction)
+        {
+            return _pool.ContainsKey(transaction.HashString);
+        }
+
+        /// <summary>
+        /// Registers a transaction to the pool.
+        /// </summary>
+        public void RegisterTransaction(Transaction transaction)
+        {
+            if (HasTransactionInChain(transaction))
+            {
+                throw new ArgumentException(
+                    "given transaction already exists in the chain");
+            }
+            else if (HasTransactionInPool(transaction))
+            {
+                throw new ArgumentException(
+                    "given transaction already exists in the pool");
+            }
+            else
+            {
+                _pool.Add(transaction.HashString, transaction);
+            }
+        }
+
+        /// <summary>
+        /// Removes a transaction from the pool. Made private so that
+        /// this can be called only when moving a transaction to a block
+        /// in the chain. This makes a registered transaction uncancellable.
+        /// </summary>
+        private void RemoveTransaction(Transaction transaction)
+        {
+            _pool.Remove(transaction.HashString);
         }
     }
 }
