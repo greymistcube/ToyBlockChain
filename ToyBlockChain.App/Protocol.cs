801af45a0738c7fe169fce73b5612d28a5f0f013
Say Cheong <greymistcube@gmail.com>
2021-04-13 15:00:58 +0900

    dummy protocol added

---
+++ ToyBlockChain.App/Protocol.cs
@@ -0,0 +1,11 @@
+namespace ToyBlockChain.App
+{
+    public class Protocol
+    {
+        public const string ANNOUNCE_ADDRESS = "AA";
+        public const string ANNOUNCE_TRANSACTION = "AT";
+        public const string ANNOUNCE_BLOCK = "AB";
+        public const string REQUEST_ROUTING_TABLE = "RR";
+        public const string REQUEST_BLOCKCHAIN = "RB";
+    }
+}
