cbdd023ff22755c8da85292ad9d9821f96e0c84a
Say Cheong <greymistcube@gmail.com>
2021-04-13 13:18:11 +0900

    basic command parsing

---
+++ ToyBlockChain.App/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using CommandLine;
+
+namespace ToyBlockChain.App
+{
+    public class Program
+    {
+        public class Options
+        {
+            [Option('s', "seed",
+                Default = false, Required = false,
+                HelpText = "Makes the node run as a seed.")]
+            public bool Seed { get; set; }
+
+            [Option('l', "logging",
+                Default = false, Required = false,
+                HelpText = "Prints output to a console.")]
+            public bool Logging { get; set; }
+
+            [Option('v', "verbose",
+                Default = false, Required = false,
+                HelpText = "Makes the output verbose.")]
+            public bool Verbose { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            Options options = new Options();
+            ParserResult<Options> result = Parser.Default
+                .ParseArguments<Options>(args)
+                .WithParsed<Options>(o => {
+                    options = o;
+                });
+            if (result.Tag == ParserResultType.NotParsed)
+            {
+                Console.WriteLine("Not Parsed");
+                return;
+            }
+
+            bool seed = options.Seed;
+            bool logging = options.Logging;
+            bool verbose = options.Verbose;
+        }
+    }
+}
