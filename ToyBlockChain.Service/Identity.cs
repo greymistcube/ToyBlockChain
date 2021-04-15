bc723a0c53dfe2f8a17f92254a77040f0c536457
Say Cheong <greymistcube@gmail.com>
2021-04-15 11:33:05 +0900

    Identity class added; requires further fix for compatibility

---
+++ ToyBlockChain.Service/Identity.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Text;
+using System.Security.Cryptography;
+using ToyBlockChain.Crypto;
+
+namespace ToyBlockChain.Service
+{
+    public class Identity
+    {
+        public const string SEPARATOR = "<ID>";
+        private RSAParameters _rsaParameters;
+        private string _publicKey;
+        private string _address;
+
+        public Identity()
+        {
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            _rsaParameters = rsa.ExportParameters(true);
+
+            string modulus = Convert.ToHexString(_rsaParameters.Modulus);
+            string exponent = Convert.ToHexString(_rsaParameters.Exponent);
+            _publicKey = $"{modulus}:{exponent}";
+            _address = CryptoUtil.ComputeHashString(_publicKey);
+        }
+
+        public string Address
+        {
+            get
+            {
+                return _address;
+            }
+        }
+
+        public string PublicKey
+        {
+            get
+            {
+                return _publicKey;
+            }
+        }
+
+        public string ToSerializedString()
+        {
+            return $"{_address}{SEPARATOR}{_publicKey}";
+        }
+
+        public byte[] ToSerializedBytes()
+        {
+            return Encoding.UTF8.GetBytes(ToSerializedString());
+        }
+    }
+}
